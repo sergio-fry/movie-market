fa3d593c5c42ca5c386e6aed924c92e68fa05e2c
Федоткин Олег Юрьевич <o.fedotkin@sq-supports-MacBook-Pro.local>
2016-11-03 09:18:26 +0300

    Added country

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,31 +1,28 @@
 require 'active_support/all'
 
 class Movie
-  attr_reader :actors, :release_date
-  
-  def initialize(title:, actors: [], release_date:)
+  attr_reader :actors, :release_date, :country
+
+  PREMIUM_ACTORS = ['Sergey Bezrukov']
+  PREMIUM_EXTRA_PRICE = 5
+
+  def initialize(title:, actors: [], release_date:, country:)
     @title = title
     @actors = actors
     @release_date = release_date
+    @country = country
   end
-  
-  PREMIUM_ACTORS = ['Sergey Bezrukov']
-  PREMIUM_EXTRA_PRICE = 5
-  
+
   def price
     has_premium_actor? ? base_price + PREMIUM_EXTRA_PRICE : base_price
   end
-  
+
   private
-  
+
   def base_price
-    if release_date < 1.years.ago
-      10
-    else
-      20
-    end
+    release_date < 1.years.ago ? 10 : 20
   end
-  
+
   def has_premium_actor?
     (actors & PREMIUM_ACTORS).any?
   end
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -3,26 +3,31 @@ require_relative '../lib/movie'
 
 describe Movie do
   subject(:movie) { Movie.new attributes }
-  
+
   let(:attributes) { default_attributes }
   let(:default_attributes) do
     {
       title: 'Some Movie',
       release_date: 3.years.ago,
-      actors: ['Steven Seagal', 'Arnold Schwarzenegger', 'Robert De Niro']
+      actors: ['Steven Seagal', 'Arnold Schwarzenegger', 'Robert De Niro'],
+      country: 'Russia'
     }
   end
-  
+
   it { is_expected.to be_a(Movie) }
-  
+
   it 'has 3 actors' do
     expect(movie.actors.size).to eq 3
   end
-  
+
   it 'has a release date' do
     expect(movie.release_date).to be_a Time
   end
-  
+
+  it 'has a contry' do
+    expect(movie.country).to be_a String
+  end
+
   context 'older than a year' do
     before do
       default_attributes.merge!({release_date: 2.years.ago})
@@ -31,22 +36,22 @@ describe Movie do
     it 'costs $10' do
       expect(movie.price).to eq 10
     end
-    
+
     it 'costs $5 more if actors include Bezrukov' do
       default_attributes[:actors] << 'Sergey Bezrukov'
       expect(movie.price).to eq 15
     end
   end
-  
+
   context 'newer than a year' do
     before do
       default_attributes.merge!({release_date: 1.month.ago})
     end
-    
+
     it 'costs $20' do
       expect(movie.price).to eq 20
     end
-    
+
     it 'costs $5 more if actors include Bezrukov' do
       default_attributes[:actors] << 'Sergey Bezrukov'
       expect(movie.price).to eq 25
